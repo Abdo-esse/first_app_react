0d38e90ec807f6eda8f453dcdb6b4d1667a8d678
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-10 09:50:35 +0100

    add firste componet

---
+++ src/holloWorld/HelloWorld.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function HelloWorld() {
+//   return (
+//     <div>
+//       hello Worled
+//     </div>
+//   )
+// }
+class HelloWorld extends React.Component{
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
+
+export default HelloWorld
