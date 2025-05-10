c2f9bf4b66de67f4f0dbc487e10d98ba2fa87347
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-10 10:40:45 +0100

    add component fruits

---
+++ src/Fruits/Fruit.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Fruit() {
+  return (
+    <>
+      <ul>
+        <li>Strawberry</li>
+        <li>lemon</li>
+        <li>Orange</li>
+        <li>Appel</li>
+      </ul>
+    </>
+  )
+}
+
+export default Fruit
