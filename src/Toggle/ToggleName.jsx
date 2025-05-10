6bb9d1d8d967c71d7afc240d81377644c0e07797
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-10 15:44:18 +0100

    add togglr name

---
+++ src/Toggle/ToggleName.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import HelloWorld from '../holloWorld/HelloWorld'
+
+export default class ToggleName extends Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld lastname="mohamed"/>
+      </div>
+    )
+  }
+}
