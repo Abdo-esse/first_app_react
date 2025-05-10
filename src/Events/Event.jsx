3302b7780ce6f4cb1bd167c87eeb8de307369548
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-10 12:04:21 +0100

    termin click event

--- src/Events/Event.jsx
+++ src/Events/Event.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import { click } from '@testing-library/user-event/dist/click'
+import React,{Component} from 'react'
 
-function Event() {
-  return (
-    <div>
-      <button>
+// function Event() {
+
+//     function handleClick(){
+//         alert("click")
+//     }
+//   return (
+//     <div>
+//       <button onClick={handleClick}>
+//         click
+//       </button>
+//     </div>
+//   )
+// }
+
+class Event extends Component {
+
+    handleClick(){
+        alert("click")
+    }
+
+    render(){
+        return(
+            <div>
+      <button onClick={this.handleClick}>
         click
-      </button>
+       </button>
     </div>
-  )
-}
+        )
+    }
+    
 
+}
 export default Event
