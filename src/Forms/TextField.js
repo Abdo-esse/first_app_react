7f06a267c5ddd8839c70b93b13fca72614de6cbf
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-10 10:02:36 +0100

    add form field

---
+++ src/Forms/TextField.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TextField() {
+  return (
+    <div>
+      <label>XXX</label>
+      <input type='text'/>
+    </div>
+  )
+}
+
+export default TextField
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HelloWorld from './holloWorld/HelloWorld';
+import TextField from './Forms/TextField';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <HelloWorld lastname="Abdo"/>
-    <HelloWorld lastname="Amid"/>
-    <HelloWorld lastname="mouad"/>
+    <TextField />
   </React.StrictMode>
 );
 
