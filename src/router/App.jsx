dfefceb699f456612d6029a76fff3909142ecd50
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-12 10:09:28 +0100

    termin router

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import Layout from './pages/Layout'
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <BrowserRouter>
      <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route index element
-
+          <Route index element={<Home/>}/>
+          <Route path='blogs' element={<Blogs/>}/>
+          <Route path='contact' element={<Contact/>}/>
         </Route>
 
      </Routes>
