09b331879fe216ecc64b0195fc1a9dc0025f6b8f
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-05-19 20:00:29 +0100

    add dtore despatche

--- src/Redux/Example.js
+++ src/Redux/Example.js
@@ -1,11 +1,14 @@
 import { createStore } from 'redux'
 import { CounterReducer } from './Reducers/Counterreducer'
 
-const store = createStore(CounterReducer)
+const store = createStore(CounterReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 store.subscribe(() => {
-    alert('state changed')
+    console.log(store.getState());
+    
 })
 
 store.dispatch({ type: 'counter/increment' })
+store.dispatch({ type: 'counter/increment' })
+store.dispatch({ type: 'counter/decrement' })
 console.log(3);
